aa7bb4ec692a8ff8be65ebfc58bfbf2c7b03ce37
Santiago Saavedra <santiagosaavedra@gmail.com>
2016-09-12 02:13:38 +0200

    Add additional logic in SchemaGenerator
    This should get responses and produces from the function.
    
    Actually the produces is hardcoded for the moment, but the _responses
    should come from a decorator.

--- drf_swagger_extras/schemas.py
+++ drf_swagger_extras/schemas.py
@@ -1,3 +1,5 @@
+from copy import copy
+
 from rest_framework.compat import urlparse
 from rest_framework.schemas import SchemaGenerator as BaseSchemaGenerator
 
@@ -22,7 +24,7 @@ class SchemaGenerator(BaseSchemaGenerator):
 
         description = self.get_description(path, method, callback, view)
 
-        return coreapi.Link(
+        link = coreapi.Link(
             url=urlparse.urljoin(self.url, path),
             action=method.lower(),
             encoding=encoding,
@@ -30,15 +32,40 @@ class SchemaGenerator(BaseSchemaGenerator):
             fields=fields,
             transform=None,  # Not handled, but here for future reference
         )
+        link._responses = self.get_responses(path, method, callback, view)
+        link._produces = self.get_produces(path, method, callback, view)
 
-    def get_description(self, path, method, callback, view):
+        return link
+
+    def _get_actual_view(self, method, callback, view, default=True):
         if hasattr(callback, 'actions'):
             action_name = callback.actions[method.lower()]
-            action = (getattr(view, action_name))
-            if action.__doc__:
-                return self._get_description(view, action)
+            action = getattr(view, action_name)
+            return action
+        else:
+            return view if default else None
 
-        return self._get_description(view, None)
+    def get_responses(self, path, method, callback, view):
+        # Get generic responses
+        responses = {}
+        if hasattr(view, '_responses'):
+            responses = copy(view._responses)
+            pass
+
+        action = self._get_actual_view(method, callback, view, default=False)
+        if action and hasattr(action, '_responses'):
+            responses.update(action._responses)
+        return responses or None
+
+    def get_produces(self, path, method, callback, view):
+        return ["application/json", "application/xml"]
+
+    def get_description(self, path, method, callback, view):
+        action = self._get_actual_view(method, callback, view, default=False)
+        if action and action.__doc__:
+            return self._get_description(view, action)
+        else:
+            return self._get_description(view, None)
 
     def _get_description(self, view, action=None):
         generic = view.__doc__
